01d38753f4e701720dbd2ffad363af94a41d569d
ilisien <ian8lisien@gmail.com>
2023-08-10 16:07:21 -0400

    message (untested)

--- cameras/message.py
+++ cameras/message.py
@@ -1,9 +1,64 @@
-class Message():
-    def __init__(self,bytes):
-        header_bytes = bytes[2:12]
+class MessageData: # root message data class
+    pass
+
+class UnicodeMessageData(MessageData):
+    def __init__(self,message_string):
+        self.message_string = message_string
+
+class ErrorMessageData(MessageData):
+    def __init__(self,error_type,error_string):
+        self.error_type = error_type
+        self.error_string = error_string
+
+class RequestImageMessageData(MessageData):
+    def __init__(self,camera_id,image_type,image_size_preset):
+        self.camera_id = camera_id
+        self.image_type = image_type
+        self.image_size_preset = image_size_preset
+
+class ImageMessageData(MessageData):
+    def __init__(self,image_type,image_size_preset,image_bytes):
+        self.image_type = image_type
+        self.image_size_preset = image_size_preset
+        self.image_bytes = image_bytes
+
+class Message:
+    def __init__(self,type,version,count,data_length,data):
+        self.type = type
+        self.version = version
+        self.count = count
+        self.data_length = data_length
+        self.data = data
+
+def parse_message(bytes):
+    header_bytes = bytes[2:12] # first two bytes are for sync
+    type = header_bytes[0].decode()
+    version = int.from_bytes(header_bytes[1])
+    count = int.from_bytes(header_bytes[2:6])
+    data_length = int.from_bytes(header_bytes[6:10]) # length after the header
+
+    if version == 0:
         message_bytes = bytes[12:]
-        self.type = header_bytes[0].decode()
-        self.version = int.from_bytes(header_bytes[1])
-        self.count = int.from_bytes(header_bytes[2:6])
-        self.data_length = int.from_bytes(header_bytes[6:10])
-        
+        if type == "u": # unicode message
+            message_string = message_bytes.decode("utf-8")
+
+            message_data = UnicodeMessageData(message_string)
+        elif type == "e": # error message
+            error_type = message_bytes[0].decode()
+            error_message = message_bytes[1:].decode("utf-8")
+
+            message_data = ErrorMessageData(error_type,error_message)
+        elif type == "r": # request image message
+            camera_id = int.from_bytes(message_bytes[0])
+            image_type = message_bytes[1].decode()
+            image_size_preset = message_bytes[2:4].decode()
+
+            message_data = RequestImageMessageData(camera_id,image_type,image_size_preset)
+        elif type == "i": # image message
+            image_type = message_bytes[0].decode()
+            image_size_preset = message_bytes[1:3].decode()
+            image_bytes = message_bytes[3:]
+
+            message_data = ImageMessageData(image_type,image_size_preset,image_bytes)
+    
+    return Message(type,version,count,data_length,message_data)
